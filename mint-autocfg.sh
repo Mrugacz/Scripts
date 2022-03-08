36f419078b0a314b018fef1503a5b8679529de3a
Mrugacz <51380638+Mrugacz@users.noreply.github.com>
2022-03-08 23:35:01 +0100

    Add files via upload

--- mint-autocfg.sh
+++ mint-autocfg.sh
@@ -3,14 +3,14 @@
 # My programs
 #
 
-sudo apt install -y bspwm sxhkd kitty nitrogen i3lock fish dmenu fonts-firacode thunar maim arandr
-
+sudo apt install -y git bspwm sxhkd kitty nitrogen i3lock fish dmenu fonts-firacode thunar maim arandr emacs
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
 #
 # polybar compile requirements
 #
 
-sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
-sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+sudo apt install -y build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
 
 cd /tmp/
 git clone --recursive https://github.com/polybar/polybar
@@ -26,15 +26,16 @@ install -Dm644 /usr/local/share/doc/polybar/config $HOME/.config/polybar/config
 
 chsh -s /usr/bin/fish
 
-mkdir ~/Scripts ~/.config/bspwm ~/.config/sxhkd
+mkdir ~/Scripts/ ~/.config/bspwm ~/.config/sxhkd ~/.config/kitty/ ~/.config/polybar/ ~/.config/fish ~/.config/fish/functions
 
 cd ~/Scripts
 wget https://raw.githubusercontent.com/JopStro/bspswallow/master/bspswallow
+wget https://raw.githubusercontent.com/Mrugacz/Scripts/main/README.org
 
 cd ~/.config
-wget --output-document bspwm/bspwmrc https://pastebin.com/raw/pjeeft7n
 echo kitty > ~/.config/bspwm/terminals
-wget --output-document sxhkd/sxhkdrc https://pastebin.com/raw/DCsS6NeW
+wget https://raw.githubusercontent.com/Mrugacz/dotfiles/main/README.org
 
-chmod +x bspwm/bspwmrc
-chmod +x sxhkd/sxhkdrc
+fish alias ssh=kitty +kitten ssh
+
+emacs
